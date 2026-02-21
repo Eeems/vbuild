290947c4856d187c3af0b072b2fcdb0fd4ef654f
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-02-21 15:04:41 -0700

    Add automatic variable check to basic test

--- tests/basic/validate.sh
+++ tests/basic/validate.sh
@@ -9,3 +9,23 @@ exists entware-rc.post-os-upgrade
 exists entware-rc.post-upgrade
 exists entware-rc.pre-deinstall
 exists dist/noarch/entware-rc-0.1-r0.apk
+if grep -q "builddir=" APKBUILD; then
+  echo '$builddir present'
+  exit 1
+fi
+if grep -q "pkgdir=" APKBUILD; then
+  echo '$pkgdir present'
+  exit 1
+fi
+if grep -q "srcdir=" APKBUILD; then
+  echo '$srcdir present'
+  exit 1
+fi
+if grep -q "startdir=" APKBUILD; then
+  echo '$startdir present'
+  exit 1
+fi
+if grep -q "subpkgdir=" APKBUILD; then
+  echo '$subpkgdir present'
+  exit 1
+fi
